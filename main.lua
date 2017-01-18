882e73b2fecd11221e8063c06c053f8ea9dd11c8
pexeen <dj22dj22dj@gmail.com>
2017-01-18 11:17:08 +0100

    Changing time to time left, adding an array of last scores

--- main.lua
+++ main.lua
@@ -3,13 +3,14 @@ function love.load()
   mouseClicks = 0
   time = 0
   lastScore = 0
-  hightScore = 0
+  highScore = 0
   screenW, screenH = love.graphics.getDimensions()
   screenWMid, screenHMid = screenW / 2, screenH / 2
   love.mouse.setVisible(false)
   r = 255
   g = 255
   b = 0
+  scores = {}
 end
 
 function love.update(dt)
@@ -22,13 +23,19 @@ function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.setNewFont(250)
+  love.graphics.setNewFont(250/1080*screenH)
   love.graphics.print("Clicks : "..mouseClicks, screenWMid -700 , screenHMid - 175)
-  love.graphics.setNewFont(40)
+  love.graphics.setNewFont(40/1080*screenH)
   local timeRounded = math.floor(time * 100 + 0.5) / 100
-  love.graphics.print("Time : "..timeRounded, 0, 0)
-  love.graphics.print("Last Score : "..lastScore, screenW - 375, 0)
-  love.graphics.print("Hight Score : "..hightScore, screenW - 400, screenH - 50)
+  timeLeft = timeLimit - timeRounded
+  love.graphics.print("Time Left : "..timeLeft, 0, 0)
+  love.graphics.print("Last Scores : ", screenW - 375, 0)
+  for i,s in ipairs(scores) do
+
+    love.graphics.print(s, screenW - 100, i*40)
+  end
+
+  love.graphics.print("High Score : "..highScore, screenW - 400, screenH - 50)
 end
 
 function love.mousepressed(x, y, button, isTouch)
@@ -44,15 +51,23 @@ function love.keypressed(key)
   end
   if key == 'r' then
     timeReset()
-    hightScore = 0
+    highScore = 0
+  end
+  if key == 'c'then
+    cheat()
   end
 end
 
 function timeReset()
   lastScore = mouseClicks
-  if lastScore >= hightScore then
-    hightScore = lastScore
+  if lastScore >= highScore then
+    highScore = lastScore
   end
+  table.insert(scores, 1, lastScore)
   time = 0
   mouseClicks = 0
+end
+
+function cheat()
+  mouseClicks = mouseClicks + love.math.random(1,4)
 end
